1e324e917c3fa4edebb24ddc205fdf16f5dfe927
Mohsen Honarmandi <42585987+shhonarmandi@users.noreply.github.com>
2025-08-26 21:28:00 +0200

    chore: test material date-picker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {MAT_DATE_LOCALE, provideNativeDateAdapter} from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    {provide: MAT_DATE_LOCALE, useValue: 'en'},
+    provideNativeDateAdapter()
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {Component, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {DateAdapter} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatFormFieldModule, MatInputModule, MatDatepickerModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-angular-app');
+  private dateAdaptor = inject(DateAdapter<Date>);
+
+  constructor() {
+    console.log('App constructor', this.dateAdaptor);
+  }
+
+  public switchToEnglish() {
+    this.dateAdaptor.setLocale('en');
+  }
+
+  public switchToGerman() {
+    this.dateAdaptor.setLocale('de');
+  }
 }
